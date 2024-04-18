7a759f53ae3c131d74d6d5599d0113ed809bf046
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-18 15:57:12 +0200

    Creates RegisterPage

---
+++ src/components/Pages/RegisterPage/index.jsx
@@ -0,0 +1,12 @@
+export default function RegisterPage() {
+  return (
+    <main className="flex flex-col gap-8">
+      <div>
+        <h1>Register</h1>
+        <p className="text-primary font-heading font-bold text-lg">a new account</p>
+      </div>
+
+      <section>Register form goes here...</section>
+    </main>
+  );
+}
