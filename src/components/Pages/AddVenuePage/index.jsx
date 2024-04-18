b3ec36eb4161849a737bc695938f75d5711dd41f
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-18 16:46:58 +0200

    Creates AddVenuePage component

---
+++ src/components/Pages/AddVenuePage/index.jsx
@@ -0,0 +1,9 @@
+export default function AddVenuePage() {
+  return (
+    <main className="flex flex-col gap-8">
+      <h1>Add new venue</h1>
+
+      <section>Add venue form goes here...</section>
+    </main>
+  );
+}
