0d23b4247eba69297b079163f17e6d9b3d03eeca
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-18 16:55:05 +0200

    Creates ContactPage component

---
+++ src/components/Pages/ContactPage/index.jsx
@@ -0,0 +1,13 @@
+export default function ContactPage() {
+  return (
+    <main className="flex flex-col gap-8">
+      <h1>Contact us</h1>
+
+      <section className="flex flex-col gap-5">
+        <p>Please send us a message if you have any questions or concern. Our normal response time is up to 48h.</p>
+
+        <div>Contact form goes here...</div>
+      </section>
+    </main>
+  );
+}
