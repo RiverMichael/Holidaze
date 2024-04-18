64e81faf0a05a5ad63e9ed3f0d64c886a8d75912
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-18 16:08:46 +0200

    Creates LoginPage

---
+++ src/components/Pages/LoginPage/index.jsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+  return (
+    <main className="flex flex-col gap-8">
+      <div>
+        <h1>Login</h1>
+        <p className="text-primary font-heading font-bold text-lg">to your account</p>
+      </div>
+
+      <section>Login form goes here...</section>
+    </main>
+  );
+}
