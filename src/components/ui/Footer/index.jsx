abb7b035bc487629915e4e434632d7f257bf3cb7
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-18 18:28:38 +0200

    Adds Links on the footer

--- src/components/ui/Footer/index.jsx
+++ src/components/ui/Footer/index.jsx
@@ -1,3 +1,4 @@
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../../../assets/holidaze_logo.png";
 
 export default function Footer() {
@@ -5,17 +6,37 @@ export default function Footer() {
     <footer className="bg-secondary md:rounded-lg md:shadow md:m-6">
       <div className="w-full  mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <figure className="mb-4 sm:mb-0">
-            <img src={Logo} alt="Holidaze logotype" className="h-8" />
-          </figure>
+          <Link to="/">
+            <figure className="mb-4 sm:mb-0">
+              <img src={Logo} alt="Holidaze logotype" className="h-8" />
+            </figure>
+          </Link>
+
           <ul className="flex flex-wrap items-center gap-3 md:gap-6 text-sm font-medium mb-6 sm:mb-0">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>
+              <NavLink to="/" className={({ isActive }) => `text-base ${isActive ? "opacity-40" : "hover:text-gray-400"}`}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="about" className={({ isActive }) => `text-base ${isActive ? "opacity-40" : "hover:text-gray-400"}`}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="contact" className={({ isActive }) => `text-base ${isActive ? "opacity-40" : "hover:text-gray-400"}`}>
+                Contact
+              </NavLink>
+            </li>
           </ul>
         </div>
         <hr className="my-6 lg:my-8 w-full rounded border border-gray-300 " />
-        <span className="block text-sm sm:text-center">Copyright &copy; 2024 | Michael Nilsson</span>
+        <span className="block text-sm sm:text-center">
+          Copyright &copy; 2024 |{" "}
+          <Link to="https://www.michaelriver.dev" target="_blank" className="hover:text-gray-400">
+            Michael Nilsson
+          </Link>
+        </span>
       </div>
     </footer>
   );
