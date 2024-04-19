76e774ada4041a8a5b6702c83ccd6a741eb006c3
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-19 18:38:30 +0200

    Styling changes in TailwindCSS

--- src/components/ui/Header/index.jsx
+++ src/components/ui/Header/index.jsx
@@ -2,7 +2,7 @@ import NavBar from "../../NavBar";
 
 export default function Header() {
   return (
-    <header className="bg-background sticky top-0 z-50 p-5 relative">
+    <header className="bg-neutral sticky top-0 z-50 p-5 relative">
       <NavBar />
     </header>
   );
