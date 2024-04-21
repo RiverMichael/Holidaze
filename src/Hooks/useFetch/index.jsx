f2ad53b930d801bbdd921fbc14f02582e849510f
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-21 14:49:35 +0200

    Creates doFetch and useDoFetch hook

---
+++ src/Hooks/useFetch/index.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import doFetch from "../../components/doFetch";
+
+export default function useDoFetch(url, options) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await doFetch(url, options);
+        setData(result);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+}
