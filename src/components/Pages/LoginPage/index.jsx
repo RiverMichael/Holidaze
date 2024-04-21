a9f43cfb5ef381339a9d1f407ac2d8bacb987250
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-21 17:30:50 +0200

    Creates LoginForm, and adds functionality to post the data and to create-api-key

--- src/components/Pages/LoginPage/index.jsx
+++ src/components/Pages/LoginPage/index.jsx
@@ -1,12 +1,16 @@
+import LoginForm from "../../LoginForm";
+
 export default function LoginPage() {
   return (
-    <main className="flex flex-col gap-8">
+    <main className="flex flex-col gap-8 px-5 container max-w-sm mx-auto">
       <div>
         <h1>Login</h1>
         <p className="text-primary font-heading font-bold text-lg">to your account</p>
       </div>
 
-      <section>Login form goes here...</section>
+      <section>
+        <LoginForm />
+      </section>
     </main>
   );
 }
