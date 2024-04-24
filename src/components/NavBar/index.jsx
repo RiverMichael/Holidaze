7c13660a0a840513e67a9f10bf407d95cd866614
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-24 19:28:49 +0200

    Displays the searchbar in the header

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -9,6 +9,7 @@ import useHandleLogout from "../../hooks/useHandleLogout";
 
 export default function NavBar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
   const { isAuthenticated, profile } = useAuth();
   const handleLogout = useHandleLogout();
 
@@ -29,96 +30,102 @@ export default function NavBar() {
   }, [isNavOpen]);
 
   return (
-    <nav className="bg-neutral">
-      <div className="w-full flex flex-wrap items-center justify-between mx-auto">
-        <button type="button" onClick={() => setIsNavOpen(true)} id="navbar-toggler" className="md:hidden">
-          <IoMenu size={30} className="text-primary" />
-        </button>
+    <>
+      <nav className="bg-neutral">
+        <div className="w-full flex flex-wrap items-center justify-between mx-auto">
+          <button type="button" onClick={() => setIsNavOpen(true)} id="navbar-toggler" className="lg:hidden">
+            <IoMenu size={30} className="text-primary" />
+          </button>
 
-        <Link to="/">
-          <img src={Logo} alt="Holidaze logotype" className="h-8" />
-        </Link>
+          <Link to="/">
+            <img src={Logo} alt="Holidaze logotype" className="h-8" />
+          </Link>
 
-        <div className="hidden md:block">
-          <SearchBar />
-        </div>
-
-        <div className="flex items-center gap-5">
-          <div
-            id="navbar-menu"
-            className={`${
-              isNavOpen ? "flex p-5 h-screen absolute left-0 top-0" : "hidden"
-            } bg-secondary md:bg-neutral md:flex flex-col md:flex-row items-center md:w-full md:p-0 md:h-full md:relative gap-5`}>
-            <div className="flex w-full justify-end">
-              <button onClick={() => setIsNavOpen(false)} className="mb-16 md:hidden" aria-label="Show menu">
-                <IoClose size={40} className="text-primary" />
-              </button>
-            </div>
-
-            <div className="flex flex-col md:flex-row gap-5 md:items-center grow justify-between px-5 mb-5 md:mb-0 md:p-0">
-              <ul className="flex flex-col gap-5 md:flex-row">
-                <li>
-                  <NavLink
-                    to="/"
-                    onClick={() => setIsNavOpen(false)}
-                    className={({ isActive }) => `font-bold text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`}>
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="about"
-                    onClick={() => setIsNavOpen(false)}
-                    className={({ isActive }) => `font-bold text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`}>
-                    About
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="contact"
-                    onClick={() => setIsNavOpen(false)}
-                    className={({ isActive }) => `font-bold text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`}>
-                    Contact
-                  </NavLink>
-                </li>
-              </ul>
-
-              {!isAuthenticated ? (
-                <div className="flex flex-col md:flex-row gap-2">
-                  <NavLink to="login" onClick={() => setIsNavOpen(false)} className="btn btn-outlined w-full">
-                    Login
-                  </NavLink>
-                  <NavLink to="register" onClick={() => setIsNavOpen(false)} className="btn btn-primary w-full">
-                    Sign up
-                  </NavLink>
-                </div>
-              ) : (
-                <div className="flex flex-col gap-5">
-                  <NavLink
-                    to="profile"
-                    onClick={() => setIsNavOpen(false)}
-                    className={({ isActive }) => `flex items-center gap-2 md:hidden text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`}>
-                    <figure className="w-10 h-10">
-                      <img src={profile.avatar} alt={`${profile.name} avatar`} className="w-full h-full rounded-full object-center object-cover" />
-                    </figure>
-                    {profile.name}
-                  </NavLink>
-
-                  <button onClick={() => handleLogout()} className="md:hidden btn btn-outlined text-sm bg-secondary">
-                    Log out
-                  </button>
-
-                  <ProfileDropdown />
-                </div>
-              )}
-            </div>
+          <div className="hidden lg:block grow">
+            <SearchBar />
           </div>
 
-          <button type="button" className="md:hidden">
-            <IoSearch size={30} className="text-primary" />
-          </button>
+          <div className="flex items-center">
+            <div
+              id="navbar-menu"
+              className={`${
+                isNavOpen ? "flex p-5 h-screen absolute left-0 top-0 z-50" : "hidden"
+              } bg-secondary lg:bg-neutral lg:flex flex-col lg:flex-row items-center lg:w-full lg:p-0 lg:h-full lg:relative gap-5`}>
+              <div className="flex w-full justify-end mb-16 lg:hidden">
+                <button onClick={() => setIsNavOpen(false)} aria-label="Close menu">
+                  <IoClose size={40} className="text-primary" />
+                </button>
+              </div>
+
+              <div className="flex flex-col lg:flex-row gap-5 lg:items-center grow justify-between px-5 mb-5 lg:mb-0 lg:p-0">
+                <ul className="flex flex-col gap-5 lg:flex-row">
+                  <li>
+                    <NavLink
+                      to="/"
+                      onClick={() => setIsNavOpen(false)}
+                      className={({ isActive }) => `font-bold text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`}>
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="about"
+                      onClick={() => setIsNavOpen(false)}
+                      className={({ isActive }) => `font-bold text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`}>
+                      About
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="contact"
+                      onClick={() => setIsNavOpen(false)}
+                      className={({ isActive }) => `font-bold text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`}>
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
+
+                {!isAuthenticated ? (
+                  <div className="flex flex-col lg:flex-row gap-2">
+                    <NavLink to="login" onClick={() => setIsNavOpen(false)} className="btn btn-outlined w-full">
+                      Login
+                    </NavLink>
+                    <NavLink to="register" onClick={() => setIsNavOpen(false)} className="btn btn-primary w-full">
+                      Sign up
+                    </NavLink>
+                  </div>
+                ) : (
+                  <div className="flex flex-col gap-5">
+                    <NavLink
+                      to="profile"
+                      onClick={() => setIsNavOpen(false)}
+                      className={({ isActive }) =>
+                        `flex items-center gap-2 lg:hidden text-text ${isActive ? "opacity-50" : "text-primary hover:opacity-50 transition-colors duration-300 ease-in-out"}`
+                      }>
+                      <figure className="w-10 h-10">
+                        <img src={profile.avatar} alt={`${profile.name} avatar`} className="w-full h-full rounded-full object-center object-cover" />
+                      </figure>
+                      {profile.name}
+                    </NavLink>
+
+                    <button onClick={() => handleLogout()} className="lg:hidden btn btn-outlined text-sm bg-secondary">
+                      Log out
+                    </button>
+
+                    <ProfileDropdown />
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <button type="button" id="search-toggler" className="lg:hidden">
+              <IoSearch size={30} className="text-primary" onClick={() => setIsSearchVisible(!isSearchVisible)} />
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <div className="w-full mt-5 lg:hidden">{isSearchVisible && <SearchBar />}</div>
+    </>
   );
 }
--- src/components/ui/Header/index.jsx
+++ src/components/ui/Header/index.jsx
@@ -2,7 +2,7 @@ import NavBar from "../../NavBar";
 
 export default function Header() {
   return (
-    <header className="bg-neutral sticky top-0 z-40 p-5 relative">
+    <header className="bg-neutral sticky top-0 z-40 p-5 relative w-full">
       <NavBar />
     </header>
   );
