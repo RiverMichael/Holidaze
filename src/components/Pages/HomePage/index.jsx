eeaae4494c2284e8d2f177c8ff1f4b62cfcd95be
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-25 14:32:46 +0200

    Styling changes

--- src/components/Pages/HomePage/index.jsx
+++ src/components/Pages/HomePage/index.jsx
@@ -2,7 +2,7 @@ import VenuesList from "../../VenuesList";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col gap-8 px-5 container mx-auto">
+    <main className="flex flex-col gap-8 px-5 container">
       <h1 className="text-center">Browse Venues</h1>
       <VenuesList />
     </main>
--- src/components/Pages/RegisterPage/index.jsx
+++ src/components/Pages/RegisterPage/index.jsx
@@ -2,7 +2,7 @@ import RegisterForm from "../../RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <main className="flex flex-col gap-8 px-5 container max-w-sm mx-auto">
+    <main className="flex flex-col gap-8 px-5 container max-w-sm">
       <div>
         <h1>Register</h1>
         <p className="text-primary font-heading font-bold text-lg">a new account</p>
