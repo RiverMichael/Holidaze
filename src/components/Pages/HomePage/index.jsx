0f3e1f7cc42a44b60ccbb7fac7a0ae4e4bcd87fb
Michael River Nilsson <mrn@michaelriver.dev>
2024-05-08 16:12:49 +0200

    Adds pagination to the VenuesList on the HomePage

--- src/components/Pages/HomePage/index.jsx
+++ src/components/Pages/HomePage/index.jsx
@@ -5,8 +5,8 @@ export default function HomePage() {
   useUpdateHead("Home", "Browse through all the amazing venues available at Holidaze");
 
   return (
-    <main className="flex flex-col gap-8 px-5 container">
-      <h1 className="text-center">Browse Venues</h1>
+    <main className="flex flex-col gap-8 px-5 container items-center">
+      <h1>Browse Venues</h1>
       <VenuesList />
     </main>
   );
--- src/components/VenuesList/index.jsx
+++ src/components/VenuesList/index.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import { API_BASE_URL } from "../../constants/apiURL";
 import useDoFetch from "../../hooks/useDoFetch";
 import VenueCard from "../VenueCard";
 import ErrorMessage from "../ui/ErrorMessage";
 import LoadingIndicator from "../ui/LoadingIndicator";
+import { Pagination } from "flowbite-react";
 
 export default function VenuesList() {
-  const { data, isLoading, isError } = useDoFetch(`${API_BASE_URL}/venues`);
+  const [isCurrentPage, setIsCurrentPage] = useState(1);
+  const { data, isLoading, isError, metaData } = useDoFetch(`${API_BASE_URL}/venues?page=${isCurrentPage}`);
   const venues = data.filter(
     (venue) => !venue.name.toLowerCase().includes("test") && !venue.name.toLowerCase().includes("string") && !venue.name.toLowerCase().includes("aa") && (venue.location.city || venue.location.country)
   );
+
+  const onPageChange = (page) => {
+    setIsCurrentPage(page);
+  };
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [isCurrentPage]);
 
   if (isLoading) {
     return <LoadingIndicator />;
@@ -20,11 +34,17 @@ export default function VenuesList() {
 
   if (venues) {
     return (
-      <section className="flex flex-wrap gap-y-10 gap-x-10 justify-center">
-        {venues.map((venue) => (
-          <VenueCard key={venue.id} venue={venue} />
-        ))}
-      </section>
+      <>
+        <section className="flex flex-wrap gap-10 justify-center w-full">
+          {venues.map((venue) => (
+            <VenueCard key={venue.id} venue={venue} />
+          ))}
+        </section>
+
+        <div className="overflow-x-auto  text-primary-light">
+          <Pagination currentPage={isCurrentPage} totalPages={metaData.pageCount} onPageChange={onPageChange} previousLabel="" nextLabel="" showIcons />
+        </div>
+      </>
     );
   }
 }
