b0c8789ec0f69190ab130fd61ea2b3d478c4fe00
Michael River Nilsson <mrn@michaelriver.dev>
2024-05-10 15:31:48 +0200

    Creates and displays ContactForm on ContactPage

--- src/components/Pages/ContactPage/index.jsx
+++ src/components/Pages/ContactPage/index.jsx
@@ -1,16 +1,20 @@
 import useUpdateHead from "../../../hooks/useUpdateHead";
+import ContactForm from "../../forms/ContactForm";
 
 export default function ContactPage() {
   useUpdateHead("Contact", "Contact us if you have any questions or concerns");
 
   return (
-    <main className="flex flex-col gap-8">
-      <h1>Contact us</h1>
+    <main className="flex flex-col gap-8 px-5 container max-w-sm">
+      <div>
+        <h1>Contact</h1>
+        <p className="text-primary font-heading font-bold text-lg">us at Holidaze</p>
+      </div>
 
       <section className="flex flex-col gap-5">
-        <p>Please send us a message if you have any questions or concern. Our normal response time is up to 48h.</p>
+        <p>Please send us a message if you have any questions or concerns. Our normal response time is up to 48h.</p>
 
-        <div>Contact form goes here...</div>
+        <ContactForm />
       </section>
     </main>
   );
