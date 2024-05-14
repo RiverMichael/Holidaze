b69546713719d16048fcc718896080bd294fbd65
Michael River Nilsson <mrn@michaelriver.dev>
2024-05-14 16:07:10 +0200

    Updates to FilterVenuesForm

--- src/components/forms/FilterVenuesForm/index.jsx
+++ src/components/forms/FilterVenuesForm/index.jsx
@@ -2,27 +2,40 @@ import { useForm } from "react-hook-form";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
-export default function FilterVenuesForm({ filters, setFilters }) {
+export default function FilterVenuesForm({ filterTerms, setFilterTerms, setIsModalOpen }) {
   const { register, handleSubmit, reset, setValue, watch } = useForm({
     defaultValues: {
-      priceRange: [50, 1000],
-      ratingRange: 0,
-      minGuests: 0,
+      priceRange: filterTerms?.priceRange || [10, 10000],
+      ratingRange: filterTerms?.ratingRange || 0,
+      minGuests: filterTerms?.minGuests || 0,
       features: {
-        wifi: false,
-        parking: false,
-        breakfast: false,
-        pets: false,
+        wifi: filterTerms?.features?.wifi || false,
+        parking: filterTerms?.features?.parking || false,
+        breakfast: filterTerms?.features?.breakfast || false,
+        pets: filterTerms?.features?.pets || false,
       },
     },
   });
+
+  const defaultValues = {
+    priceRange: [10, 10000],
+    ratingRange: 0,
+    minGuests: 0,
+    features: {
+      wifi: false,
+      parking: false,
+      breakfast: false,
+      pets: false,
+    },
+  };
 
   const priceRange = watch("priceRange");
   const ratingRange = watch("ratingRange");
 
   const handleFilterFormSubmit = (data) => {
     const filterData = { ...data, minGuests: parseInt(data.minGuests) };
-    setFilters(filterData);
+    setFilterTerms(filterData);
+    setIsModalOpen(false);
   };
 
   const handlePriceRangeChange = (newRange) => {
@@ -34,11 +47,12 @@ export default function FilterVenuesForm({ filters, setFilters }) {
   };
 
   const handleClearFilters = () => {
-    reset();
+    setFilterTerms(defaultValues);
+    reset(defaultValues);
   };
 
   return (
-    <form id="filterForm" onSubmit={handleSubmit(handleFilterFormSubmit)} className="flex flex-col gap-5">
+    <form id="filterForm" onSubmit={handleSubmit(handleFilterFormSubmit)} className="flex flex-col gap-10">
       <div className="w-full flex flex-col gap-1">
         <label htmlFor="priceRange" className="flex text-primary text-lg font-bold w-full justify-between items-center">
           Price range
@@ -61,7 +75,15 @@ export default function FilterVenuesForm({ filters, setFilters }) {
         <h4 className="text-lg font-body text-primary">Number of guests</h4>
         <ul className="flex flex-wrap gap-2">
           <li>
-            <input type="radio" name="minGuests" id="guestsAny" value={0} {...register("minGuests")} className="hidden peer" defaultChecked />
+            <input
+              type="radio"
+              name="minGuests"
+              id="guestsAny"
+              value={0}
+              {...register("minGuests")}
+              className="hidden peer"
+              defaultChecked={filterTerms?.minGuests === undefined || filterTerms?.minGuests === 0}
+            />
             <label
               htmlFor="guestsAny"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -69,7 +91,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests1" value={1} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests1" value={1} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 1} />
             <label
               htmlFor="guests1"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -77,7 +99,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests2" value={2} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests2" value={2} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 2} />
             <label
               htmlFor="guests2"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -85,7 +107,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests3" value={3} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests3" value={3} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 3} />
             <label
               htmlFor="guests3"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -93,7 +115,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests4" value={4} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests4" value={4} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 4} />
             <label
               htmlFor="guests4"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -101,7 +123,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests5" value={5} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests5" value={5} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 5} />
             <label
               htmlFor="guests5"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -109,7 +131,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests6" value={6} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests6" value={6} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 6} />
             <label
               htmlFor="guests6"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -117,7 +139,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests7" value={7} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests7" value={7} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 7} />
             <label
               htmlFor="guests7"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -125,7 +147,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
             </label>
           </li>
           <li>
-            <input type="radio" name="minGuests" id="guests8" value={8} {...register("minGuests")} className="hidden peer" />
+            <input type="radio" name="minGuests" id="guests8" value={8} {...register("minGuests")} className="hidden peer" defaultChecked={filterTerms?.minGuests === 8} />
             <label
               htmlFor="guests8"
               className="px-3 py-1 rounded-full bg-neutral text-primary border border-primary hover:bg-primary-light hover:text-neutral hover:border-primary-light peer-checked:bg-primary peer-checked:text-neutral text-sm cursor-pointer">
@@ -189,7 +211,7 @@ export default function FilterVenuesForm({ filters, setFilters }) {
         <button type="submit" className="btn btn-primary w-1/2">
           Apply filters
         </button>
-        <button onClick={handleClearFilters} className="btn btn-outlined w-1/4">
+        <button type="button" onClick={handleClearFilters} className="btn btn-outlined w-1/4">
           Clear
         </button>
       </div>
