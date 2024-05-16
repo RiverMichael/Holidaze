9ad71bd20cfe5d8df9930822c129bda66fee0baf
Michael River Nilsson <mrn@michaelriver.dev>
2024-05-16 10:48:28 +0200

    Installs and Configures Vitest for unit-testing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test.setup.js",
+    exclude: [...configDefaults.exclude],
+  },
   define: {
     "process.env": {},
   },
